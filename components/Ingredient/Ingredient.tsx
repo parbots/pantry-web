d7212e0027f928bd23010dd319a4ee6e2c3080e7
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-26 12:46:43 -0500

    Create Shelf and Ingredient components

---
+++ components/Ingredient/Ingredient.tsx
@@ -0,0 +1,14 @@
+
+import styles from './Ingredient.module.css'
+import React from 'react';
+
+const Ingredient = () => {
+    return (
+        <div>
+            <h1>Ingredient</h1>
+        </div>
+    );
+};
+
+export default Ingredient;
+
---
+++ components/Shelf/Shelf.tsx
@@ -0,0 +1,14 @@
+
+import styles from './Shelf.module.css'
+import React from 'react';
+
+const Shelf = () => {
+    return (
+        <div>
+            <h1>Shelf</h1>
+        </div>
+    );
+};
+
+export default Shelf;
+
